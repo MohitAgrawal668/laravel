f3581d0d1501851633d4c359c88f41ef1fbc678a
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-25 15:22:46 +0530

    we use a methoda validation and used required attribute in this and print these error under every input element

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -9,24 +9,40 @@
         <div class="form-group">
           <label for="">Name</label>
           <input type="text" name="name" id="name" class="form-control" placeholder="" aria-describedby="helpId">
-          <small id="helpId" class="text-muted"></small>
+          <small id="helpId" class="text-danger">
+            @error("name")
+                {{ $message }}
+            @enderror
+          </small>
         </div>
         
         <div class="form-group">
             <label for="">Email</label>
             <input type="text" name="email" id="email" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted"></small>
+            <small id="helpId" class="text-danger">
+                @error("email")
+                    {{ $message }}
+                @enderror
+            </small>
         </div>
         
         <div class="form-group">
             <label for="">Password</label>
             <input type="text" name="password" id="password" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted"></small>
+            <small id="helpId" class="text-danger">
+                @error("password")
+                    {{ $message }}
+                @enderror
+            </small>
         </div>
         <div class="form-group">
             <label for="">Confirm Password</label>
             <input type="text" name="confirm-password" id="confirm-password" class="form-control" placeholder="" aria-describedby="helpId">
-            <small id="helpId" class="text-muted"></small>
+            <small id="helpId" class="text-danger">
+                @error("confirm-password")
+                    {{ $message }} 
+                @enderror
+            </small>
         </div>
         <button type="submit" class="btn btn-primary" name="">Submit</button>  
     </form>
