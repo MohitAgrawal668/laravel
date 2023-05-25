26eeb20115bf8aa5af0b0fc12c68a8561c2d25f0
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-25 13:50:06 +0530

    learn how to create controller and how to hit a function from route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,14 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get("/",function(){
-        return view("home");
-    });
-
-Route::get("/about",function(){
-    return view("about");
-});
-
-Route::get("/contact", function(){
-    return view("contact");
-});
+Route::get("/",[HomeController::class, 'index']);
+Route::get("/about", [HomeController::class, 'about']);
