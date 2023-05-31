74f3eeec75f1b6b2e47509dd5985fbcb814b3e45
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-31 18:39:08 +0530

    we just crated two tables and relate them with foreign key with migration

---
+++ database/migrations/2023_05_31_130102_create_client_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",100);
+            $table->string("mobile",20);
+            $table->unsignedBigInteger("group_id");
+            $table->foreign("group_id")->references("id")->on("client_group");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client');
+    }
+};
